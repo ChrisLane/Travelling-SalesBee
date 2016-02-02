4008322437c5eba8fa10290e5a28f8b9dee8b0f2
mxm499 <mxm499@7a56c92f-03fb-4445-8927-fcf652c9610f>
2016-02-02 22:58:26 +0000

    Added get and set methods along with CELL_TYPE, x, y - Melvyn

--- src/main/java/com/teamc2/travellingsalesbee/gui/elements/ElementCell.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/elements/ElementCell.java
@@ -3,13 +3,65 @@ package com.teamc2.travellingsalesbee.gui.elements;
 import javafx.geometry.Point2D;
 
 public class ElementCell extends Point2D implements Element{
+	
+	private static int CELL_TYPE = 0; //0 for empty cell, 1 for hive, 2 for flower/node
+	private int x; //x position of Cell
+	private int y; //y position of Cell
+	
     /**
      * Creates a new instance of {@code Point2D}.
      *
      * @param x the x coordinate of the point
      * @param y the y coordinate of the point
      */
-    public ElementCell(double x, double y) {
+    public ElementCell(double x, double y, int type) {
         super(x, y);
+        this.x = x; //x position of object
+        this.y = y; //y position of object
+        
+        CELL_TYPE = type; //type of object (see above for types)
+    }
+    
+    //GET METHODS
+    public int getLoc() {
+    	return (x, y);
+    }
+    
+    public int getType() {
+    	return CELL_TYPE; //We could change this to return a string later on
+    }
+    
+    public String getType() {
+    	if(CELL_TYPE == 0) {
+    		return "Empty Cell";
+    	} else if(CELL_TYPE == 1) {
+    		return "Hive";
+    	} else if(CELL_TYPE == 2) {
+    		return "Flower";
+    	} else {
+    		return "Error";
+    	}
+    }
+    
+    public getX() {
+    	return this.x;
+    }
+    
+    public getImage() {
+    	return this.y;
+    }
+    
+    //SET METHODS
+    public void setLoc(int x, int y) {
+    	this.x = x;
+    	this.y = y;
+    }
+    
+    public void setX(int x) {
+    	this.x = x;
+    }
+    
+    public void setY(int y) {
+    	this.y = y;
     }
 }
