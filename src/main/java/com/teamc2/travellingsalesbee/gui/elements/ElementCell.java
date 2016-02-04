9ceabd0f4f1c8552b10bc54fe7a5a7df7c0fca3c
Chris Lane <ChrisLane@users.noreply.github.com>
2016-02-04 12:59:46 +0000

    Tabs should be tab characters and not spaces

--- src/main/java/com/teamc2/travellingsalesbee/gui/elements/ElementCell.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/elements/ElementCell.java
@@ -2,52 +2,56 @@ package com.teamc2.travellingsalesbee.gui.elements;
 
 import javafx.geometry.Point2D;
 
-public class ElementCell extends Point2D implements Element{
-	
+public class ElementCell extends Point2D implements Element {
+
 	private static int CELL_TYPE = 0; //0 for empty cell, 1 for hive, 2 for flower/node
 	private int x; //x position of Cell
 	private int y; //y position of Cell
-	
-    /**
-     * Creates a new instance of {@code Point2D}.
-     *
-     * @param x the x coordinate of the point
-     * @param y the y coordinate of the point
-     */
-    public ElementCell(int x, int y, int type) {
-        super(x, y);
-        this.x = x; //x position of object
-        this.y = y; //y position of object
-        
-        CELL_TYPE = type; //type of object (see above for types)
-    }
-    
-    //GET METHODS
 
-    public String getType() {
-        switch (CELL_TYPE) {
-            case 0: return "Empty Cell";
-            case 1: return "Hive";
-            case 2: return "Flower";
-            default: return "Error, please enter 0, 1 or 2";
-        }
-    }
-    
-    public String getImage() {
-    	return "";
-    }
-    
-    //SET METHODS
-    public void setLoc(int x, int y) {
-    	this.x = x;
-    	this.y = y;
-    }
-    
-    public void setX(int x) {
-    	this.x = x;
-    }
-    
-    public void setY(int y) {
-    	this.y = y;
-    }
+	/**
+	 * Creates a new instance of {@code Point2D}.
+	 *
+	 * @param x the x coordinate of the point
+	 * @param y the y coordinate of the point
+	 */
+	public ElementCell(int x, int y, int type) {
+		super(x, y);
+		this.x = x; //x position of object
+		this.y = y; //y position of object
+
+		CELL_TYPE = type; //type of object (see above for types)
+	}
+
+	//GET METHODS
+
+	public String getType() {
+		switch (CELL_TYPE) {
+			case 0:
+				return "Empty Cell";
+			case 1:
+				return "Hive";
+			case 2:
+				return "Flower";
+			default:
+				return "Error, please enter 0, 1 or 2";
+		}
+	}
+
+	public String getImage() {
+		return "";
+	}
+
+	//SET METHODS
+	public void setLoc(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
 }
--- src/main/java/com/teamc2/travellingsalesbee/gui/elements/ElementSettings.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/elements/ElementSettings.java
@@ -1,17 +1,17 @@
 package com.teamc2.travellingsalesbee.gui.elements;
 
 public class ElementSettings implements Element {
-    private ElementMap gridMap;
+	private ElementMap gridMap;
 
-    public ElementSettings(ElementMap gridMap) {
-        this.gridMap = gridMap;
-    }
+	public ElementSettings(ElementMap gridMap) {
+		this.gridMap = gridMap;
+	}
 
-    public void setSpeed(int speed) {
-        gridMap.setSpeed(speed);
-    }
+	public void setSpeed(int speed) {
+		gridMap.setSpeed(speed);
+	}
 
-    public void resetMap() {
-        gridMap.resetMap();
-    }
+	public void resetMap() {
+		gridMap.resetMap();
+	}
 }
