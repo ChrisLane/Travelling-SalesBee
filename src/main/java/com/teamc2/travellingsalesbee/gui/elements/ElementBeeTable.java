6c36d06313d765c2c35cfba65ec52d46ba7f7e55
cml476 <cml476@7a56c92f-03fb-4445-8927-fcf652c9610f>
2016-02-04 13:59:39 +0000

    Add methods for filling the bee table

--- src/main/java/com/teamc2/travellingsalesbee/gui/elements/ElementBeeTable.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/elements/ElementBeeTable.java
@@ -1,4 +1,16 @@
 package com.teamc2.travellingsalesbee.gui.elements;
 
+import java.util.ArrayList;
+
 public class ElementBeeTable implements Element {
+	private ElementMap map;
+
+	public ElementBeeTable(ElementMap map) {
+		this.map = map;
+	}
+
+	public void fillTable() {
+		ArrayList<ElementCell> cells = map.getCells();
+		// TODO: implement 'fillTable' method
+	}
 }
