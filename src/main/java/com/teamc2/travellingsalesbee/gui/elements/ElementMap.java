92676127c0d26a22df9713637354f026a94b2d79
cml476 <cml476@7a56c92f-03fb-4445-8927-fcf652c9610f>
2016-02-04 12:59:46 +0000

    Tabs should be tab characters and not spaces

--- src/main/java/com/teamc2/travellingsalesbee/gui/elements/ElementMap.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/elements/ElementMap.java
@@ -4,55 +4,55 @@ import java.util.ArrayList;
 
 public class ElementMap implements Element {
 
-    private int width; //Width of map
-    private int height; //Height of map
-    private int speed; //Speed of bees
-    private ArrayList<ElementCell> cells; //Will store our ElementCells
+	private int width; //Width of map
+	private int height; //Height of map
+	private int speed; //Speed of bees
+	private ArrayList<ElementCell> cells; //Will store our ElementCells
 
-    /**
-     * Create a new map
-     *
-     * @param width
-     * @param height
-     */
-    public ElementMap(int width, int height) {
-        this.width = width;
-        this.height = height;
-    }
-    
-   	//GET METHODS
-   
-    public getCellType(ElementCell cell) {
-    	return cell.getType();
-    }
-    
-    public ArrayList<ElementCell> getCellsOfType() {
-        // TODO: Implement 'getCellsOfType' method
-        return null;
-    }
-    
-    //SET METHODS
-    
-    /**
-     * Adds a new cell to the grid map at x, y
-     */
-    public void newCell(int x, int y) {
-    	ElementCell nCell = new ElementCell();
-    	cells.add(nCell);
-        // TODO: Implement 'addCell' method
-    }
+	/**
+	 * Create a new map
+	 *
+	 * @param width
+	 * @param height
+	 */
+	public ElementMap(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
 
-    /**
-     * Sets the speed for the bees to move at
-     */
-    public void setSpeed(int speed) {
-        // TODO: Implement 'setSpeed' method
-    }
+	//GET METHODS
 
-    /**
-     * Reset the current map
-     */
-    public void resetMap() {
-        // TODO: Implement 'resetMap' method
-    }
+	public getCellType(ElementCell cell) {
+		return cell.getType();
+	}
+
+	public ArrayList<ElementCell> getCellsOfType() {
+		// TODO: Implement 'getCellsOfType' method
+		return null;
+	}
+
+	//SET METHODS
+
+	/**
+	 * Adds a new cell to the grid map at x, y
+	 */
+	public void newCell(int x, int y) {
+		ElementCell nCell = new ElementCell();
+		cells.add(nCell);
+		// TODO: Implement 'addCell' method
+	}
+
+	/**
+	 * Sets the speed for the bees to move at
+	 */
+	public void setSpeed(int speed) {
+		// TODO: Implement 'setSpeed' method
+	}
+
+	/**
+	 * Reset the current map
+	 */
+	public void resetMap() {
+		// TODO: Implement 'resetMap' method
+	}
 }
