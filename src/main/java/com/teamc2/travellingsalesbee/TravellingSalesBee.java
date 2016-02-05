dd9a19ca9a3ed69072f9eb80befb9d09c0e0a646
Chris Lane <ChrisLane@users.noreply.github.com>
2016-02-05 11:22:37 +0000

    Setup main method and class

---
+++ src/main/java/com/teamc2/travellingsalesbee/TravellingSalesBee.java
@@ -0,0 +1,9 @@
+package com.teamc2.travellingsalesbee;
+
+import com.teamc2.travellingsalesbee.gui.GUI;
+
+public class TravellingSalesBee {
+	public static void main(String[] args) {
+		GUI gui = new GUI();
+	}
+}
--- src/main/java/com/teamc2/travellingsalesbee/gui/GUI.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/GUI.java
@@ -1,4 +1,19 @@
 package com.teamc2.travellingsalesbee.gui;
 
+import com.teamc2.travellingsalesbee.gui.elements.ElementBeeTable;
+import com.teamc2.travellingsalesbee.gui.elements.ElementMap;
+import com.teamc2.travellingsalesbee.gui.elements.ElementSettings;
+import com.teamc2.travellingsalesbee.gui.elements.ElementToolbox;
+
 public class GUI {
+	public GUI() {
+		initialise();
+	}
+
+	public void initialise() {
+		ElementMap map = new ElementMap(10, 10);
+		ElementSettings settings = new ElementSettings(map);
+		ElementBeeTable beeTable = new ElementBeeTable(map);
+		ElementToolbox toolbox = new ElementToolbox();
+	}
 }
