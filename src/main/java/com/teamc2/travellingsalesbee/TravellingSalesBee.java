647cccb3513460c49e25f61d73d67b37f910a14f
txw467 <txw467@7a56c92f-03fb-4445-8927-fcf652c9610f>
2016-02-08 12:37:42 +0000

    Changed references from 'Gui' to 'GUI'

--- src/main/java/com/teamc2/travellingsalesbee/TravellingSalesBee.java
+++ src/main/java/com/teamc2/travellingsalesbee/TravellingSalesBee.java
@@ -1,9 +1,9 @@
 package com.teamc2.travellingsalesbee;
 
-import com.teamc2.travellingsalesbee.gui.Gui;
+import com.teamc2.travellingsalesbee.gui.GUI;
 
 public class TravellingSalesBee {
 	public static void main(String[] args) {
-		Gui gui = new Gui();
+		GUI gui = new GUI();
 	}
 }
