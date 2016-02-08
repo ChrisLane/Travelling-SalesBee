beecff6d7171b7af54ed6c7835f741d56819785f
txw467 <tnttodda@users.noreply.github.com>
2016-02-08 12:15:14 +0000

    Changed Cells to extend Point and implement Cell

--- src/main/java/com/teamc2/travellingsalesbee/gui/elements/cells/Cell.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/elements/cells/Cell.java
@@ -1,15 +1,13 @@
 package com.teamc2.travellingsalesbee.gui.elements.cells;
 
 public interface Cell {
+	
 	String EMPTY = "Empty";
 	String FLOWER = "Flower";
-	String Hive = "Hive";
+	String HIVE = "Hive";
 
 	String getType();
 
 	String getImage();
-	
-	int getX();
-	
-	int getY();
+
 }
--- src/main/java/com/teamc2/travellingsalesbee/gui/elements/cells/CellFlower.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/elements/cells/CellFlower.java
@@ -1,32 +1,21 @@
 package com.teamc2.travellingsalesbee.gui.elements.cells;
 
-public class CellFlower implements Cell {
-	
-	private int x;
-	private int y;
+import java.awt.Point;
+
+@SuppressWarnings("serial")
+public class CellFlower extends Point implements Cell {
 	
 	public CellFlower(int x, int y){
-		this.x=x;
-		this.y=y;
+		super(x,y);
 	}
 	
-	@Override
 	public String getType() {
 		return Cell.FLOWER;
 	}
 
-	@Override
 	public String getImage() {
 		//return flower image location here
 		return null;
 	}
-	
-	@Override
-	public int getX() {
-		return this.x;
-	}
-	@Override
-	public int getY() {
-		return this.y;
-	}
+
 }
