9c88df92b5bf301f6406d2e764f2e963275bfa45
txw467 <txw467@7a56c92f-03fb-4445-8927-fcf652c9610f>
2016-02-08 12:15:14 +0000

    Changed Cells to extend Point and implement Cell

--- src/main/java/com/teamc2/travellingsalesbee/gui/elements/cells/CellEmpty.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/elements/cells/CellEmpty.java
@@ -1,29 +1,20 @@
 package com.teamc2.travellingsalesbee.gui.elements.cells;
 
-public class CellEmpty implements Cell {
-	
-	private int x;
-	private int y;
+import java.awt.Point;
+
+@SuppressWarnings("serial")
+public class CellEmpty extends Point implements Cell {
 	
 	public CellEmpty(int x, int y){
-		this.x=x;
-		this.y=y;
+		super(x,y);
 	}
-	@Override
+	
 	public String getType() {
 		return Cell.EMPTY;
 	}
 
-	@Override
 	public String getImage() {
 		return null;
 	}
-	@Override
-	public int getX() {
-		return this.x;
-	}
-	@Override
-	public int getY() {
-		return this.y;
-	}
+
 }
--- src/main/java/com/teamc2/travellingsalesbee/gui/elements/cells/CellHive.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/elements/cells/CellHive.java
@@ -1,32 +1,21 @@
 package com.teamc2.travellingsalesbee.gui.elements.cells;
 
-public class CellHive implements Cell {
-	
-	private int x;
-	private int y;
+import java.awt.Point;
+
+@SuppressWarnings("serial")
+public class CellHive extends Point implements Cell {
 	
 	public CellHive(int x, int y){
-		this.x=x;
-		this.y=y;
+		super(x,y);
 	}
 	
-	@Override
 	public String getType() {
-		return Cell.Hive;
+		return Cell.HIVE;
 	}
 
-	@Override
 	public String getImage() {
 		//return hive image location here
 		return null;
 	}
-	
-	@Override
-	public int getX() {
-		return this.x;
-	}
-	@Override
-	public int getY() {
-		return this.y;
-	}
+
 }
