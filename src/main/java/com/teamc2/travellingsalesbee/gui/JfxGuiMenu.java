76f32e3d241e7b1725241b737ea4d64bff31b17e
npf489 <npf489@7a56c92f-03fb-4445-8927-fcf652c9610f>
2016-02-11 13:10:19 +0000

    Refactor menu code

--- src/main/java/com/teamc2/travellingsalesbee/gui/JfxGuiMenu.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/JfxGuiMenu.java
@@ -5,42 +5,32 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class JfxGuiMenu extends Application
-{
-	public static void main(String[] args)
-	{
+public class JfxGuiMenu extends Application {
+	protected final int appWidth = 700;
+	protected final int appHeight = 768;
+
+	public static void main(String[] args) {
 		launch(args);
 	}
 
-	public void start(Stage stage)
-	{
+	public void start(Stage stage) {
 		BorderPane border = new BorderPane();
 		border.setId("main");
 
 		VBox vBox = new VBox();
 
-		Button simButton = new Button("Run Simulation");
-		simButton.setMinSize(150, 60);
-		simButton.getStyleClass().add("button");
-
-		Button docButton = new Button("Documentation");
-		docButton.setMinSize(150, 60);
-		docButton.getStyleClass().add("button");
-
-		Button faqButton = new Button("FAQ");
-		faqButton.setMinSize(150, 60);
-		faqButton.getStyleClass().add("button");
-
-		Button aboutButton = new Button("About");
-		aboutButton.setMinSize(150, 60);
-		aboutButton.getStyleClass().add("button");
+		Button simButton = createButton("Run Simulation");
+		Button docButton = createButton("Documentation");
+		Button faqButton = createButton("FAQ");
+		Button aboutButton = createButton("About");
 
 		vBox.getChildren().addAll(simButton, docButton, faqButton, aboutButton);
 		vBox.setAlignment(Pos.TOP_CENTER);
@@ -49,20 +39,30 @@ public class JfxGuiMenu extends Application
 
 		border.setCenter(vBox);
 
-		Scene scene = new Scene(border, 700, 768);
+		Scene scene = new Scene(border, appWidth, appHeight);
 		File file = new File("target/classes/stylesheets/menu.css");
 		try {
 			URL url = file.toURI().toURL();
 			scene.getStylesheets().add(url.toExternalForm());
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
+			System.exit(1);
 		}
 		stage.setScene(scene);
 		stage.setTitle("Travelling Salesbee");
-		stage.setMinHeight(768);
-		stage.setMaxHeight(768);
-		stage.setMinWidth(700);
-		stage.setMaxWidth(700);
+		stage.setMinHeight(appHeight);
+		stage.setMaxHeight(appHeight);
+		stage.setMinWidth(appWidth);
+		stage.setMaxWidth(appWidth);
 		stage.show();
+	}
+
+	protected Button createButton(String title)
+	{
+		Button button = new Button(title);
+		button.setMinSize(150, 60);
+		button.getStyleClass().add("button");
+
+		return button;
 	}
 }
