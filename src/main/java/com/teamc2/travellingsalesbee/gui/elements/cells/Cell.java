5e1503806441c722fed42c35cb4918ad4c3a8a89
Chris Lane <ChrisLane@users.noreply.github.com>
2016-02-12 15:45:16 +0000

    Make Cell abstract

--- src/main/java/com/teamc2/travellingsalesbee/gui/elements/cells/Cell.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/elements/cells/Cell.java
@@ -3,7 +3,7 @@ package com.teamc2.travellingsalesbee.gui.elements.cells;
 import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 
-public class Cell extends Point2D.Double {
+public abstract class Cell extends Point2D.Double {
 
 	public enum CellType {EMPTY, FLOWER, HIVE};
 	
