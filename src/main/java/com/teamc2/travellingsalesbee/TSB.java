22c06f61bef5626bd37717f25804e8fe5f799888
cml476 <cml476@7a56c92f-03fb-4445-8927-fcf652c9610f>
2016-02-15 11:58:07 +0000

    Fix formatting of TSB. We're using tab characters.

--- src/main/java/com/teamc2/travellingsalesbee/TSB.java
+++ src/main/java/com/teamc2/travellingsalesbee/TSB.java
@@ -1,19 +1,15 @@
 package com.teamc2.travellingsalesbee;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.EventQueue;
-import java.awt.Image;
-import java.awt.Toolkit;
-import java.io.File;
-import java.io.IOException;
-import java.util.Observable;
-import java.util.Observer;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Observable;
+import java.util.Observer;
 
 public class TSB extends JFrame implements Observer {
 
@@ -62,21 +58,21 @@ public class TSB extends JFrame implements Observer {
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 				gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-						.addComponent(panel_toolbox, GroupLayout.PREFERRED_SIZE, 140, GroupLayout.PREFERRED_SIZE)
-						.addPreferredGap(ComponentPlacement.RELATED)
-						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addComponent(panel_settings, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 584, Short.MAX_VALUE)
-								.addComponent(panel_gridmap, GroupLayout.DEFAULT_SIZE, 584, Short.MAX_VALUE)))
-				);
+						.addGroup(gl_contentPane.createSequentialGroup()
+								.addComponent(panel_toolbox, GroupLayout.PREFERRED_SIZE, 140, GroupLayout.PREFERRED_SIZE)
+								.addPreferredGap(ComponentPlacement.RELATED)
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addComponent(panel_settings, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 584, Short.MAX_VALUE)
+										.addComponent(panel_gridmap, GroupLayout.DEFAULT_SIZE, 584, Short.MAX_VALUE)))
+		);
 		gl_contentPane.setVerticalGroup(
 				gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-						.addComponent(panel_gridmap, GroupLayout.DEFAULT_SIZE, 318, Short.MAX_VALUE)
-						.addPreferredGap(ComponentPlacement.RELATED)
-						.addComponent(panel_settings, GroupLayout.DEFAULT_SIZE, 116, Short.MAX_VALUE))
-				.addComponent(panel_toolbox, GroupLayout.DEFAULT_SIZE, 440, Short.MAX_VALUE)
-				);
+						.addGroup(gl_contentPane.createSequentialGroup()
+								.addComponent(panel_gridmap, GroupLayout.DEFAULT_SIZE, 318, Short.MAX_VALUE)
+								.addPreferredGap(ComponentPlacement.RELATED)
+								.addComponent(panel_settings, GroupLayout.DEFAULT_SIZE, 116, Short.MAX_VALUE))
+						.addComponent(panel_toolbox, GroupLayout.DEFAULT_SIZE, 440, Short.MAX_VALUE)
+		);
 
 		panel_gridmap = genGrid(panel_gridmap);
 
@@ -98,10 +94,10 @@ public class TSB extends JFrame implements Observer {
 		CellDrag hiveToolCell = new CellDrag("", width, height, "HIVE");
 		try {
 			Image flowerImg = ImageIO.read(new File("target/classes/icons/Flower.png"));
-			Image scaledFlowerImg = flowerImg.getScaledInstance( width, height,  java.awt.Image.SCALE_SMOOTH ) ;
+			Image scaledFlowerImg = flowerImg.getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH);
 
 			Image hiveImg = ImageIO.read(new File("target/classes/icons/Hive.png"));
-			Image scaledHiveImg = hiveImg.getScaledInstance( width, height,  java.awt.Image.SCALE_SMOOTH ) ;
+			Image scaledHiveImg = hiveImg.getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH);
 
 
 			flowerToolCell.setIcon(new ImageIcon(scaledFlowerImg));
@@ -112,61 +108,61 @@ public class TSB extends JFrame implements Observer {
 		flowerToolCell.setPanel(panel_gridmap);
 		flowerToolCell.setBounds(0, 150, width, height);
 		hiveToolCell.setPanel(panel_gridmap);
-		hiveToolCell.setBounds(0, 155+height, width, height);
+		hiveToolCell.setBounds(0, 155 + height, width, height);
 		panel_toolbox.add(flowerToolCell);
 		panel_toolbox.add(hiveToolCell);
 		System.out.println(panel_gridmap.getX());
 
-        /*****BACKGROUNDS*****/
-        //Adding background to panel_gridmap and tiling it using a nested for loop
-        Image background;
-        try {
-            background = ImageIO.read(new File("target/classes/backgrounds/grass.jpg"));
-            addTiledBgImg(panel_gridmap, background, 256, 172);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		/*****BACKGROUNDS*****/
+		//Adding background to panel_gridmap and tiling it using a nested for loop
+		Image background;
+		try {
+			background = ImageIO.read(new File("target/classes/backgrounds/grass.jpg"));
+			addTiledBgImg(panel_gridmap, background, 256, 172);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        //Background Images for Toolbox
-        Image toolbox;
-        try {
-            toolbox = ImageIO.read(new File("target/classes/backgrounds/brownBack150.png"));
-            addTiledBgImg(panel_toolbox, toolbox, 150, 150);
+		//Background Images for Toolbox
+		Image toolbox;
+		try {
+			toolbox = ImageIO.read(new File("target/classes/backgrounds/brownBack150.png"));
+			addTiledBgImg(panel_toolbox, toolbox, 150, 150);
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        //Background image for Settings
-        Image settings;
-        try {
-            settings = ImageIO.read(new File("target/classes/backgrounds/greyBack150.png"));
-            addTiledBgImg(panel_settings, settings, 150, 150);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		//Background image for Settings
+		Image settings;
+		try {
+			settings = ImageIO.read(new File("target/classes/backgrounds/greyBack150.png"));
+			addTiledBgImg(panel_settings, settings, 150, 150);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        //Background image for mother panel
-        contentPane.setBackground(new Color(71, 35, 35));
+		//Background image for mother panel
+		contentPane.setBackground(new Color(71, 35, 35));
 
 		GroupLayout gl_panel_toolbox = new GroupLayout(panel_toolbox);
 		gl_panel_toolbox.setHorizontalGroup(
 				gl_panel_toolbox.createParallelGroup(Alignment.LEADING)
-				.addComponent(txtrDragElementsOnto, GroupLayout.DEFAULT_SIZE, 140, Short.MAX_VALUE)
-				);
+						.addComponent(txtrDragElementsOnto, GroupLayout.DEFAULT_SIZE, 140, Short.MAX_VALUE)
+		);
 		gl_panel_toolbox.setVerticalGroup(
 				gl_panel_toolbox.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_toolbox.createSequentialGroup()
-						.addComponent(txtrDragElementsOnto, GroupLayout.PREFERRED_SIZE, 81, GroupLayout.PREFERRED_SIZE)
-						.addPreferredGap(ComponentPlacement.RELATED)
-						.addContainerGap(330, Short.MAX_VALUE))
-				);
+						.addGroup(gl_panel_toolbox.createSequentialGroup()
+								.addComponent(txtrDragElementsOnto, GroupLayout.PREFERRED_SIZE, 81, GroupLayout.PREFERRED_SIZE)
+								.addPreferredGap(ComponentPlacement.RELATED)
+								.addContainerGap(330, Short.MAX_VALUE))
+		);
 		panel_toolbox.setLayout(gl_panel_toolbox);
 		contentPane.setLayout(gl_contentPane);
 
 		
 		/*frame.addComponentListener(new ComponentListener() {
-		    public void componentResized(ComponentEvent e) {
+			public void componentResized(ComponentEvent e) {
 		        // do stuff           
 		    	System.out.println("Window is re-sized");
 		    }
@@ -188,7 +184,7 @@ public class TSB extends JFrame implements Observer {
 		});*/
 	}
 
-	public JPanel genGrid(JPanel panel_gridmap){
+	public JPanel genGrid(JPanel panel_gridmap) {
 		int widthCount = 0; //Keeps track of current horizontal line we're drawing
 		int heightCount = 0;//Keeps track of current vertical we're drawing
 
@@ -197,11 +193,11 @@ public class TSB extends JFrame implements Observer {
 		int screenWidth = screenSize.width;
 		int screenHeight = screenSize.height;
 
-        //Custom translucent colour
-        Color lineColor = new Color(255, 255, 255, 65);
+		//Custom translucent colour
+		Color lineColor = new Color(255, 255, 255, 65);
 
 		//While the widthCount is less than the width of the users screen, draw horizontal lines
-		while (widthCount < screenWidth){
+		while (widthCount < screenWidth) {
 
 			@SuppressWarnings("serial")
 			GridLine gridLine = new GridLine(widthCount, 0, widthCount, Integer.MAX_VALUE);
@@ -213,7 +209,7 @@ public class TSB extends JFrame implements Observer {
 		}
 
 		//While the heightCount is less than the height of the users screen, draw vertical lines
-		while (heightCount < screenHeight){
+		while (heightCount < screenHeight) {
 
 
 			@SuppressWarnings("serial")
@@ -227,17 +223,17 @@ public class TSB extends JFrame implements Observer {
 		return panel_gridmap;
 	}
 
-    public static void addTiledBgImg(JPanel panel, Image img, int width, int height) { //Adds tiles image to panel and returns it
-        for(int x = 0; x < 3840; x += width) {
-            for(int y = 0; y < 2160; y+= height) {
-                JLabel bg = new JLabel(new ImageIcon(img));
+	public static void addTiledBgImg(JPanel panel, Image img, int width, int height) { //Adds tiles image to panel and returns it
+		for (int x = 0; x < 3840; x += width) {
+			for (int y = 0; y < 2160; y += height) {
+				JLabel bg = new JLabel(new ImageIcon(img));
 
-                bg.setBounds(x, y, width, height); //grass.jpg has dimensions of: 257 x 172
-                panel.add(bg);
-            }
-        }
+				bg.setBounds(x, y, width, height); //grass.jpg has dimensions of: 257 x 172
+				panel.add(bg);
+			}
+		}
 
-    }
+	}
 
 	@Override
 	public void update(Observable o, Object arg) {
