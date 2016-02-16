d9d4a6f5f6118d60fa2e2a7f3598fe609368f60c
npf489 <npf489@7a56c92f-03fb-4445-8927-fcf652c9610f>
2016-02-16 12:22:59 +0000

    And the FAQ page

--- src/main/java/com/teamc2/travellingsalesbee/gui/pages/FaqPage.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/pages/FaqPage.java
@@ -1,5 +1,7 @@
 package com.teamc2.travellingsalesbee.gui.pages;
 
 public class FaqPage extends Page {
-	//
+	public FaqPage(String title, int height, int width) {
+		super(title, height, width);
+	}
 }
