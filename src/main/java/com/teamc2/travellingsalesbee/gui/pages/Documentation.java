8c49a62d0d2faeb45f41990217a955eb25e7bd0d
npf489 <npf489@7a56c92f-03fb-4445-8927-fcf652c9610f>
2016-02-16 12:22:16 +0000

    Update Documentation page constructor to aPage

--- src/main/java/com/teamc2/travellingsalesbee/gui/pages/Documentation.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/pages/Documentation.java
@@ -1,5 +1,7 @@
 package com.teamc2.travellingsalesbee.gui.pages;
 
 public class Documentation extends Page {
-	//
+	public Documentation(String title, int height, int width) {
+		super(title, height, width);
+	}
 }
