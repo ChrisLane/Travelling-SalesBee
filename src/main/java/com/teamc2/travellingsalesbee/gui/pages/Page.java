a338771695341f5bc0c05fba8b9c5d322f4e6987
npf489 <npf489@7a56c92f-03fb-4445-8927-fcf652c9610f>
2016-02-16 12:16:14 +0000

    Move scene creation to abstract Page

--- src/main/java/com/teamc2/travellingsalesbee/gui/pages/Page.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/pages/Page.java
@@ -1,6 +1,12 @@
 package com.teamc2.travellingsalesbee.gui.pages;
 
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public abstract class Page extends Stage {
 	protected int height;
@@ -12,5 +18,20 @@ public abstract class Page extends Stage {
 		this.height = height;
 		this.width = width;
 		setTitle(title);
+	}
+
+	protected Scene createScene(BorderPane borderPane)
+	{
+		Scene scene = new Scene(borderPane, width, height);
+		File file = new File("target/classes/stylesheets/menu.css");
+		try {
+			URL url = file.toURI().toURL();
+			scene.getStylesheets().add(url.toExternalForm());
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		return scene;
 	}
 }
