7cf7ed198d5896db9ad76cb6fd161660ba68999e
Chris Lane <ChrisLane@users.noreply.github.com>
2016-02-23 13:14:23 +0000

    Added: JavaDoc to GridLine class

--- src/main/java/com/teamc2/travellingsalesbee/gui/GridLine.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/GridLine.java
@@ -11,6 +11,14 @@ public class GridLine extends JPanel {
 	private final float x2;
 	private final float y2;
 
+	/**
+	 * Create a new line
+	 *
+	 * @param x1 X of position 1
+	 * @param y1 Y of position 1
+	 * @param x2 X of position 2
+	 * @param y2 Y of position 2
+	 */
 	public GridLine(float x1, float y1, float x2, float y2) {
 		this.x1 = x1;
 		this.x2 = x2;
@@ -18,6 +26,9 @@ public class GridLine extends JPanel {
 		this.y2 = y2;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void paintComponent(Graphics g) {
 
@@ -27,6 +38,9 @@ public class GridLine extends JPanel {
 		g2.draw(new Line2D.Float(x1, y1, x2, y2));
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public Dimension getPreferredSize() {
 		return new Dimension(300, 300);
