f7b90d8a49566f5e0f699df7f54b96e345cef33a
Chris Lane <ChrisLane@users.noreply.github.com>
2016-02-23 13:51:55 +0000

    Added: JavaDoc to Cell class

--- src/main/java/com/teamc2/travellingsalesbee/gui/elements/cells/Cell.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/elements/cells/Cell.java
@@ -5,12 +5,28 @@ import java.awt.image.BufferedImage;
 
 public abstract class Cell extends Point2D.Double {
 
+	/**
+	 * Create a new cell
+	 *
+	 * @param x X position of cell
+	 * @param y Y position of cell
+	 */
 	public Cell(double x, double y) {
 		super(x, y);
 	}
 
+	/**
+	 * Get the type of the cell
+	 *
+	 * @return Type of the cell
+	 */
 	public abstract CellType getType();
 
+	/**
+	 * Get the cell image
+	 *
+	 * @return Cell image
+	 */
 	protected abstract BufferedImage getImage();
 
 	public enum CellType {EMPTY, FLOWER, HIVE}
