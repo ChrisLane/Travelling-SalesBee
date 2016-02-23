efc83fa18f823ee35309ab528e85cc8c88a811a9
Chris Lane <ChrisLane@users.noreply.github.com>
2016-02-23 14:04:10 +0000

    Added: JavaDoc to CellComparator class

--- src/main/java/com/teamc2/travellingsalesbee/gui/elements/cells/CellComparator.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/elements/cells/CellComparator.java
@@ -3,11 +3,19 @@ package com.teamc2.travellingsalesbee.gui.elements.cells;
 import java.util.Comparator;
 
 public class CellComparator implements Comparator<Cell> {
+
+	/**
+	 * Compares and returns the order of two cells
+	 *
+	 * @param c1 First cell in comparison
+	 * @param c2 Second cell in comparison
+	 * @return Position of cell 1 compared to cell 2
+	 */
 	@Override
-	public int compare(Cell o1, Cell o2) {
-		int xComparison = Integer.compare((int) o1.x, (int) o2.x);
+	public int compare(Cell c1, Cell c2) {
+		int xComparison = Integer.compare((int) c1.x, (int) c2.x);
 		if (xComparison == 0) {
-			return Integer.compare((int) o1.y, (int) o2.y);
+			return Integer.compare((int) c1.y, (int) c2.y);
 		} else {
 			return xComparison;
 		}
