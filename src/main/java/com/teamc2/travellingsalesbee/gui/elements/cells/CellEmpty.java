28890fc06cc751a5ab05550f8c772b617c18dd8d
Chris Lane <ChrisLane@users.noreply.github.com>
2016-02-23 14:05:33 +0000

    Added: JavaDoc to CellEmpty class

--- src/main/java/com/teamc2/travellingsalesbee/gui/elements/cells/CellEmpty.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/elements/cells/CellEmpty.java
@@ -4,15 +4,27 @@ import java.awt.image.BufferedImage;
 
 public class CellEmpty extends Cell {
 
+	/**
+	 * Create a new empty cell
+	 *
+	 * @param x X position of cell
+	 * @param y Y position of cell
+	 */
 	public CellEmpty(double x, double y) {
 		super(x, y);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public CellType getType() {
 		return CellType.EMPTY;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public BufferedImage getImage() {
 		return null;
