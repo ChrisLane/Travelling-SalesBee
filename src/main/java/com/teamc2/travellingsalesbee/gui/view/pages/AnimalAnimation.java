bd74264e27bcf00cc372e9ad944c9e460a5d8b1a
mxm499 <Sefree6@users.noreply.github.com>
2016-02-26 22:16:56 +0000

    AnimationAnimation is the new class to handle animation for our sprites while traversing along paths, but I can't seem to get it to draw to the motherpanel properly...

---
+++ src/main/java/com/teamc2/travellingsalesbee/gui/view/pages/AnimalAnimation.java
@@ -0,0 +1,66 @@
+package com.teamc2.travellingsalesbee.gui.view.pages;
+
+import com.teamc2.travellingsalesbee.gui.NaiveStep;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class AnimalAnimation extends JPanel {
+
+	private double beePosX = 200, beePosY = 200,incrementX = 0, incrementY = 0;
+	private String url;
+	private NaiveStep move;
+	private JPanel panel;
+
+	private Graphics2D graphics;
+
+	public AnimalAnimation(JPanel panel, String url, NaiveStep move, Graphics2D g) {
+		this.panel = panel;
+		this.url = url;
+		this.move = move;
+		this.graphics = g;
+
+		System.out.println("aAnimation constructor");
+
+		this.paint(g);
+
+		panel.add(this);
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+
+		super.paintComponent(g);
+
+		Graphics2D g2 = this.graphics;
+
+		System.out.println("Drawing Bees");
+
+
+		try {
+			BufferedImage beeImg = ImageIO.read(this.getClass().getResource(url));
+			TexturePaint paint = new TexturePaint(beeImg, new Rectangle((int)(beePosX + incrementX), (int)(beePosY + incrementY), 50, 50));
+			g2.setPaint(paint);
+			g2.fill(new Rectangle((int)beePosX, (int)beePosY, 50, 50));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		(new Thread() {
+			public void run() {
+				try {
+					Thread.sleep(100);
+				} catch(Exception e) {
+					e.printStackTrace();
+				}
+
+				repaint();
+			}
+		}).start();
+
+	}
+}
