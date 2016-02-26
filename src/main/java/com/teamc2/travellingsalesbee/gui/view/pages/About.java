20cce7cfd44a30999a45a76af6fd9265f0b2260b
Chris Lane <ChrisLane@users.noreply.github.com>
2016-02-26 17:25:51 +0000

    Add the rest of the files from refactor

---
+++ src/main/java/com/teamc2/travellingsalesbee/gui/view/pages/About.java
@@ -0,0 +1,41 @@
+package com.teamc2.travellingsalesbee.gui.view.pages;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+/**
+ * A class for generating and viewing the about page.
+ */
+public class About extends Page {
+
+	/**
+	 * Creates a page as a stage, with a given window title, window height, and window width.
+	 *
+	 * @param title  The window title.
+	 * @param height The window height.
+	 * @param width  The window width.
+	 * @see Page#Page(String, int, int)
+	 */
+	public About(String title, int height, int width) {
+		super(title, height, width);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void bootstrap() {
+		// create the border pane
+		BorderPane border = new BorderPane();
+		border.setId("about");
+
+		// create the scene
+		Scene scene = createScene(border);
+
+		// stage-ify!
+		setScene(scene);
+		setTitle("Travelling Salesbee");
+		setMinHeight(height);
+		setMinWidth(width);
+		setResizable(false);
+	}
+}
---
+++ src/main/java/com/teamc2/travellingsalesbee/gui/view/pages/Page.java
@@ -0,0 +1,43 @@
+package com.teamc2.travellingsalesbee.gui.view.pages;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+/**
+ * Base page class.
+ */
+public abstract class Page extends Stage implements PageInterface {
+	protected final int height;
+	protected final int width;
+
+	/**
+	 * Creates a page as a stage, with a given window title, window height, and window width.
+	 *
+	 * @param title  The window title.
+	 * @param height The window height.
+	 * @param width  The window width.
+	 */
+	public Page(String title, int height, int width) {
+		super();
+		this.height = height;
+		this.width = width;
+		setTitle(title);
+	}
+
+	/**
+	 * Create the scene for the stage.
+	 *
+	 * @param borderPane The BorderPane to use in the scene.
+	 * @return The newly created scene.
+	 */
+	protected Scene createScene(BorderPane borderPane) {
+		Scene scene = new Scene(borderPane, width, height);
+		URL url = this.getClass().getResource("/assets/stylesheets/menu.css");
+		scene.getStylesheets().add(url.toExternalForm());
+
+		return scene;
+	}
+}
