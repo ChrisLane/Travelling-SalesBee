d964847d804a502507eb179c162f084f4d19512a
cml476 <cml476@7a56c92f-03fb-4445-8927-fcf652c9610f>
2016-02-27 01:25:38 +0000

    Removed: Unused import

--- src/main/java/com/teamc2/travellingsalesbee/gui/view/pages/AnimalAnimation.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/view/pages/AnimalAnimation.java
@@ -7,7 +7,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class AnimalAnimation extends JPanel {
 
