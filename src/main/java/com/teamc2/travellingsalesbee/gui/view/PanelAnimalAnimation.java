8fe2a0103fd828719e4df183589b4fa26ee13a30
mxm499 <mxm499@7a56c92f-03fb-4445-8927-fcf652c9610f>
2016-02-29 21:29:55 +0000

    PanelAnimalAnimation is now implemented into PanelMap successfully.
    Next is:
    
    -Make background transparant
    -Feed in positions for bee to fly to/from

---
+++ src/main/java/com/teamc2/travellingsalesbee/gui/view/PanelAnimalAnimation.java
@@ -0,0 +1,114 @@
+package com.teamc2.travellingsalesbee.gui.view;
+
+import java.awt.*;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javafx.animation.TranslateTransition;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.*;
+import javafx.scene.image.*;
+import javafx.scene.image.Image;
+import javafx.scene.paint.*;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.*;
+import javafx.scene.text.Font;
+import javafx.util.Duration;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+public class PanelAnimalAnimation extends JPanel {
+
+	String url;
+	int x, y, width, height;
+
+
+
+	/**
+	 *
+	 * @param x X position of panel
+	 * @param y Y position of panel
+	 * @param width Width of panel
+	 * @param height Height of panel
+	 */
+	public PanelAnimalAnimation(int x, int y, int width, int height) {
+
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+
+		initialize();
+	}
+
+	public Container initialize(){
+
+		final JFXPanel fxPanel = new JFXPanel();
+
+		this.add(fxPanel);
+		this.setVisible(true);
+
+		//Size of parent-panel
+		this.setPreferredSize(new Dimension(width, height));
+		//this.setBackground(Color.CYAN);
+
+		Platform.runLater(new Runnable() {
+			@Override
+			public void run() {
+				initFX(fxPanel);
+			}
+		});
+
+		return this;
+	}
+
+	private void initFX(JFXPanel fxPanel) {
+		Scene scene = initScene();
+		fxPanel.setScene(scene);
+	}
+
+	private Scene initScene() {
+		Group  root  =  new  Group();
+		Scene  scene  =  new  Scene(root, width, height);
+
+		final Circle circle = createRectangle();
+		final TranslateTransition transition = new TranslateTransition((Duration.seconds(0.25)), circle);
+		moveCircleOnMousePress(scene, circle, transition);
+		root.getChildren().add(circle);
+
+		url = "/assets/icons/SalesBee.png";
+
+		Image image = new Image(url);
+		circle.setFill(new ImagePattern(image, 0, 0, 1, 1, true));
+
+		/*Text  text  =  new  Text();
+		text.setX(40);
+		text.setY(100);
+		text.setFont(new Font(25));
+		text.setText("Welcome JavaFX!");
+		root.getChildren().add(text);*/
+
+		return (scene);
+	}
+
+	private Circle createRectangle() {
+		final Circle circle = new Circle(50, 250, 80);
+		circle.setOpacity(1);
+		return circle;
+	}
+
+	private void moveCircleOnMousePress(Scene scene, final Circle circle, final TranslateTransition transition) {
+		scene.setOnMousePressed(event -> {
+			transition.setToX(event.getSceneX() - circle.getCenterX());
+			transition.setToY(event.getSceneY() - circle.getCenterY());
+			transition.playFromStart();
+		});
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+}
