9bb32f5eea00c431de6baaa53576fc3bff3496cb
cml476 <cml476@7a56c92f-03fb-4445-8927-fcf652c9610f>
2016-03-02 13:40:31 +0000

    Remove unused methods, variable assignments and "this" from LayoutGui

--- src/main/java/com/teamc2/travellingsalesbee/gui/view/LayoutGui.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/view/LayoutGui.java
@@ -5,78 +5,34 @@ import java.awt.*;
 
 public class LayoutGui extends GroupLayout {
 
-	Container host;
-	PanelMap panelMap;
-	PanelSettings panelSettings;
-	PanelToolbox panelToolbox;
-	//PanelAnimalAnimation panelAnimation;
-
 	/**
 	 * Create a layout for the GUI JPanel
 	 *
-	 * @param host The container
-	 * @param panelMap The map panel
+	 * @param host          The container
+	 * @param panelMap      The map panel
 	 * @param panelSettings The settings panel
-	 * @param panelToolbox The toolbox panel
+	 * @param panelToolbox  The toolbox panel
 	 */
-	public LayoutGui(Container host, PanelMap panelMap, PanelSettings panelSettings, PanelToolbox panelToolbox/*, PanelAnimalAnimation panelAnimation*/) {
+	public LayoutGui(Container host, PanelMap panelMap, PanelSettings panelSettings, PanelToolbox panelToolbox) {
 		super(host);
-
-		this.host = host;
-		this.panelMap = panelMap;
-		this.panelSettings = panelSettings;
-		this.panelToolbox = panelToolbox;
 
 		setHorizontalGroup(
 				createParallelGroup(Alignment.LEADING)
 						.addGroup(createSequentialGroup()
-								.addComponent(this.panelToolbox, GroupLayout.PREFERRED_SIZE, 135, GroupLayout.PREFERRED_SIZE)
+								.addComponent(panelToolbox, GroupLayout.PREFERRED_SIZE, 135, GroupLayout.PREFERRED_SIZE)
 								.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
 								.addGroup(createParallelGroup(Alignment.LEADING)
-										.addComponent(this.panelSettings, GroupLayout.DEFAULT_SIZE, 789, Short.MAX_VALUE)
-										.addComponent(this.panelMap, GroupLayout.DEFAULT_SIZE, 789, Short.MAX_VALUE)))
+										.addComponent(panelSettings, GroupLayout.DEFAULT_SIZE, 789, Short.MAX_VALUE)
+										.addComponent(panelMap, GroupLayout.DEFAULT_SIZE, 789, Short.MAX_VALUE)))
 		);
 		setVerticalGroup(
 				createParallelGroup(Alignment.LEADING)
 						.addGroup(createSequentialGroup()
-								.addComponent(this.panelMap, GroupLayout.PREFERRED_SIZE, 446, GroupLayout.PREFERRED_SIZE)
+								.addComponent(panelMap, GroupLayout.PREFERRED_SIZE, 446, GroupLayout.PREFERRED_SIZE)
 								.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-								.addComponent(this.panelSettings, GroupLayout.PREFERRED_SIZE, 188, GroupLayout.PREFERRED_SIZE)
+								.addComponent(panelSettings, GroupLayout.PREFERRED_SIZE, 188, GroupLayout.PREFERRED_SIZE)
 								.addContainerGap(105, Short.MAX_VALUE))
 						.addComponent(panelToolbox, GroupLayout.DEFAULT_SIZE, 745, Short.MAX_VALUE)
 		);
-
-	}
-
-	/**
-	 *
-	 * @return Host dimensions
-	 */
-	public Dimension getHostDimensions () {
-		return new Dimension(host.getWidth(), host.getHeight());
-	}
-
-	/**
-	 *
-	 * @return Panel map dimensions
-	 */
-	public Dimension getPanelMapDimensions() {
-		return new Dimension(panelMap.getWidth(), panelMap.getHeight());
-	}
-
-	/**
-	 *
-	 * @return Panel settings dimensions
-	 */
-	public Dimension getPanelSettingsDimensions() {
-		return new Dimension(panelSettings.getWidth(), panelSettings.getHeight());
-	}
-
-	/**
-	 *
-	 * @return Panel toolbox dimensions
-	 */
-	public Dimension getPanelToolboxDimensions() {
-		return new Dimension(panelSettings.getWidth(), panelSettings.getHeight());
 	}
 }
