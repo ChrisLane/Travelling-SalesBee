9087ca822d8a3f1e872472afa4133eb99da016c0
Chris Lane <ChrisLane@users.noreply.github.com>
2016-03-02 14:00:18 +0000

    Extract the toolbox layout to its own class

---
+++ src/main/java/com/teamc2/travellingsalesbee/gui/view/LayoutToolbox.java
@@ -0,0 +1,30 @@
+package com.teamc2.travellingsalesbee.gui.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LayoutToolbox extends GroupLayout {
+	/**
+	 * Creates a layout for the toolbox container
+	 *
+	 * @param host Container for the layout to be applied to
+	 * @param backButton Back button for the toolbox
+	 */
+	public LayoutToolbox(Container host, JButton backButton) {
+		super(host);
+
+		setHorizontalGroup(
+				createParallelGroup(Alignment.LEADING)
+						.addGroup(createSequentialGroup()
+								.addContainerGap()
+								.addComponent(backButton)
+								.addContainerGap(327, Short.MAX_VALUE))
+		);
+		setVerticalGroup(
+				createParallelGroup(Alignment.LEADING)
+						.addGroup(createSequentialGroup()
+								.addComponent(backButton)
+								.addContainerGap(214, Short.MAX_VALUE))
+		);
+	}
+}
