5c3b45560f3f87fac0507067a202563d1b8162ea
txw467 <tnttodda@users.noreply.github.com>
2016-03-03 11:59:48 +0000

    Made CostMatrix symmetrical

--- src/main/java/com/teamc2/travellingsalesbee/algorithms/Bee.java
+++ src/main/java/com/teamc2/travellingsalesbee/algorithms/Bee.java
@@ -2,7 +2,6 @@ package com.teamc2.travellingsalesbee.algorithms;
 
 import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
-
 import com.teamc2.travellingsalesbee.gui.data.Map;
 import com.teamc2.travellingsalesbee.gui.data.cells.Cell;
 
