dc1a5c2575c56b3e36730e22ef7c882520c468e0
cml476 <cml476@7a56c92f-03fb-4445-8927-fcf652c9610f>
2016-03-03 12:22:21 +0000

    Fix CostEntry isKey logic (added parentheses)

--- src/main/java/com/teamc2/travellingsalesbee/algorithms/CostEntry.java
+++ src/main/java/com/teamc2/travellingsalesbee/algorithms/CostEntry.java
@@ -15,8 +15,7 @@ public class CostEntry {
 	}
 	
 	public boolean isKey(Cell cell1, Cell cell2) {
-		return (first == cell1 && second == cell2
-				|| first == cell2 && second == cell1);
+		return ((first == cell1 && second == cell2) || (first == cell2 && second == cell1));
 	}
 	
 	public double getCost() {
