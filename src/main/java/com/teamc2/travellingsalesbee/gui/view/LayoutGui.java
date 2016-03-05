4f757001c3b9bd45bbfa218757df61446f53572a
bmr455 <bmr455@7a56c92f-03fb-4445-8927-fcf652c9610f>
2016-03-05 22:40:30 +0000

    Added tabs to the visualiser and java doc'd

--- src/main/java/com/teamc2/travellingsalesbee/gui/view/LayoutGui.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/view/LayoutGui.java
@@ -1,7 +1,20 @@
 package com.teamc2.travellingsalesbee.gui.view;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Image;
+import java.awt.Insets;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.GroupLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.LayoutStyle;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.plaf.basic.BasicTabbedPaneUI;
 
 public class LayoutGui extends GroupLayout {
 
@@ -15,7 +28,43 @@ public class LayoutGui extends GroupLayout {
 	 */
 	public LayoutGui(Container host, PanelMap panelMap, PanelSettings panelSettings, PanelToolbox panelToolbox) {
 		super(host);
-
+		
+		UIManager.getDefaults().put("TabbedPane.contentBorderInsets", new Insets(0,0,0,0));
+	    UIManager.getDefaults().put("TabbedPane.tabAreaInsets", new Insets(0,0,0,0));
+	    UIManager.getDefaults().put("TabbedPane.tabsOverlapBorder", true);
+	    UIManager.put("TabbedPane.contentOpaque", false);
+		
+	    // TO DO CSS FOR TABS
+	    String tabCSS = "";	
+	    
+		JTabbedPane algorithmTabs = new JTabbedPane();
+		algorithmTabs.setTabPlacement(JTabbedPane.LEFT);
+		
+		try {
+			@SuppressWarnings("unused")
+			Image image;
+			image = ImageIO.read(this.getClass().getResource("/assets/icons/SalesBee.png"));
+			algorithmTabs.setUI(new BasicTabbedPaneUI());
+			JLabel lbl = new JLabel("Bee");
+			lbl.setVerticalTextPosition(SwingConstants.BOTTOM);
+			lbl.setHorizontalTextPosition(SwingConstants.CENTER);
+			algorithmTabs.add(getHtmlForTitle("Bee", "SalesBee.png"),panelMap);
+			algorithmTabs.add(getHtmlForTitle("Ant", "SalesBee.png"),new JPanel());
+			algorithmTabs.add(getHtmlForTitle("Nearest Neighbour", "SalesBee.png"),new JPanel());
+			algorithmTabs.add(getHtmlForTitle("2-opt swap", "SalesBee.png"), new JPanel());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		//Set background color of tabs to background color of visualiser
+		for (int i=0; i<algorithmTabs.getTabCount();i++){
+			algorithmTabs.setBackgroundAt(i, new Color(71, 35, 35));
+		}
+		
+		
+	
+		
+		
 		setHorizontalGroup(
 				createParallelGroup(Alignment.LEADING)
 						.addGroup(createSequentialGroup()
@@ -23,16 +72,26 @@ public class LayoutGui extends GroupLayout {
 								.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
 								.addGroup(createParallelGroup(Alignment.LEADING)
 										.addComponent(panelSettings, GroupLayout.DEFAULT_SIZE, 789, Short.MAX_VALUE)
-										.addComponent(panelMap, GroupLayout.DEFAULT_SIZE, 789, Short.MAX_VALUE)))
+										.addComponent(algorithmTabs, GroupLayout.DEFAULT_SIZE, 789, Short.MAX_VALUE)))
 		);
 		setVerticalGroup(
 				createParallelGroup(Alignment.LEADING)
 						.addGroup(createSequentialGroup()
-								.addComponent(panelMap, GroupLayout.PREFERRED_SIZE, 446, GroupLayout.PREFERRED_SIZE)
+								.addComponent(algorithmTabs, GroupLayout.PREFERRED_SIZE, 446, GroupLayout.PREFERRED_SIZE)
 								.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
 								.addComponent(panelSettings, GroupLayout.PREFERRED_SIZE, 188, GroupLayout.PREFERRED_SIZE)
 								.addContainerGap(105, Short.MAX_VALUE))
 						.addComponent(panelToolbox, GroupLayout.DEFAULT_SIZE, 745, Short.MAX_VALUE)
 		);
+	}
+
+	/**
+	 * 
+	 * @param text The text to present on the tab
+	 * @param imgName The image to make as a tab icon
+	 * @return htmlString The html string to produce the tab
+	 */
+	private String getHtmlForTitle(String text, String imgName) {
+		return "<html><head><style></style></head><body><div class='circleBase'><center><img src='"+this.getClass().getResource("/assets/icons/"+imgName)+"' height='50' width='50'/><br/><h2 color='#ffffff'>"+text+"</h2></div></body></html>";
 	}
 }
