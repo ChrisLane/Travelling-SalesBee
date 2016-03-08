024da7fa9e9e14edaafe6f1a4e532b1b2e8b4db4
bmr455 <bmr455@7a56c92f-03fb-4445-8927-fcf652c9610f>
2016-03-08 01:43:46 +0000

    Amended tabs so it now changes colour and image of a tab when selected

--- src/main/java/com/teamc2/travellingsalesbee/gui/view/LayoutGui.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/view/LayoutGui.java
@@ -14,6 +14,7 @@ public class LayoutGui extends GroupLayout {
 	private PanelMap panelMap;
 	private PanelToolbox panelToolbox;
 	private Color backgroundColor = new Color(71, 35, 35);
+	private Color tabColor = new Color(68, 35, 35);
 
 	/**
 	 * Create a layout for the GUI JPanel
@@ -60,26 +61,26 @@ public class LayoutGui extends GroupLayout {
 	}
 
 	private void initialiseTabs() {
+		
 		UIManager.getDefaults().put("TabbedPane.contentBorderInsets", new Insets(0, 0, 0, 0));
 		UIManager.getDefaults().put("TabbedPane.tabAreaInsets", new Insets(0, 0, 0, 0));
 		UIManager.getDefaults().put("TabbedPane.contentOpaque", false);
-		UIManager.getDefaults().put("TabbedPane.selected", this.backgroundColor);
 		UIManager.getDefaults().put("TabbedPane.tabsOpaque", false);
 		UIManager.getDefaults().put("TabbedPane.shadow", this.backgroundColor);
 		UIManager.getDefaults().put("TabbedPane.light", this.backgroundColor);
-		UIManager.getDefaults().put("TabbedPane.selectHighlight", this.backgroundColor);
 		UIManager.getDefaults().put("TabbedPane.darkShadow", this.backgroundColor);
 		UIManager.getDefaults().put("TabbedPane.focus", this.backgroundColor);
 		UIManager.getDefaults().put("TabbedPane.lightHighlight", this.backgroundColor);
 		UIManager.getDefaults().put("TabbedPane.tabsOverlapBorder", false);
 		UIManager.getDefaults().put("TabbedPane.borderColor", this.backgroundColor);
 		UIManager.getDefaults().put("TabbedPane.highlight", this.backgroundColor);
-		UIManager.getDefaults().put("TabbedPane.focus", this.backgroundColor);
 		UIManager.getDefaults().put("TabbedPane.selectHighlight", this.backgroundColor);
 		UIManager.getDefaults().put("TabbedPane.borderHightlightColor", this.backgroundColor);
 
 		// TO DO CSS FOR TABS
 		String tabCSS = "";
+		
+		
 
 		tabbedPane = new JTabbedPane();
 		tabbedPane.setTabPlacement(JTabbedPane.LEFT);
@@ -96,32 +97,51 @@ public class LayoutGui extends GroupLayout {
 			lbl.setVerticalTextPosition(SwingConstants.BOTTOM);
 			lbl.setHorizontalTextPosition(SwingConstants.CENTER);
 
-			tabbedPane.add(getHtmlForTitle("BEE", "SalesBee.png"),panelMap);
+			tabbedPane.add(getHtmlForSelectedTitle("BEE", "SalesBeeSelected.png"),panelMap);
 			tabbedPane.add(getHtmlForTitle("ANT", "Ant.png"),tabbedPane.getTabComponentAt(0));
 			tabbedPane.add(getHtmlForTitle("NN", "Mailvan.png"),tabbedPane.getTabComponentAt(0));
 			tabbedPane.add(getHtmlForTitle("2-OPT", "Mailvan.png"), tabbedPane.getTabComponentAt(0));
 
+			
+			tabbedPane.setBackgroundAt(0, this.tabColor);
+			
 			panelMap.setAlgorithmType(AlgorithmType.BEE);
+			
 
 			//Change listener to change algorithms when switching tabs
 			tabbedPane.addChangeListener(evt -> {
 				//TO DO - IVEN ART
 				int selected = tabbedPane.getSelectedIndex();
+				String text = "BEE";
+				String imgName = "SalesBee.png";
+				updateTabColours();
+				
+				
+				
 				AlgorithmType type = null;
 				switch (selected) {
 					case 0:
 						type = AlgorithmType.BEE;
+						text = "BEE";
+						imgName = "SalesBeeSelected.png";
 						break;
 					case 1:
 						type = AlgorithmType.ANT;
+						text = "ANT";
+						imgName = "AntSelected.png";
 						break;
 					case 2:
 						type = AlgorithmType.NEARESTNEIGHBOUR;
+						text = "NN";
+						imgName = "MailvanSelected.png";
 						break;
 					case 3:
 						type = AlgorithmType.TWOOPT;
+						text = "2-OPT";
+						imgName = "MailvanSelected.png";
 						break;
 				}
+				tabbedPane.setTitleAt(selected, getHtmlForSelectedTitle(text ,imgName));
 				panelMap.setAlgorithmType(type);
 				panelToolbox.setAlgorithmType(type);
 				panelMap.repaint();
@@ -141,5 +161,17 @@ public class LayoutGui extends GroupLayout {
 	 */
 	private String getHtmlForTitle(String text, String imgName) {
 		return "<html><head><style></style></head><body><div class='circleBase'><center><img src='" + this.getClass().getResource("/assets/icons/" + imgName) + "' height='50' width='50'/><br/><h2 color='#ffffff'>" + text + "</h2></div></body></html>";
+	}
+	
+	private String getHtmlForSelectedTitle(String text, String imgName) {
+		return "<html><head><style></style></head><body><div class='circleBase'><center><img src='" + this.getClass().getResource("/assets/icons/" + imgName) + "' height='50' width='50'/><br/><h2 color='#FF0000'>" + text + "</h2></div></body></html>";
+	}
+	
+	private void updateTabColours(){
+		for (int i=0; i<tabbedPane.getTabCount();i++){
+			String newTitle = tabbedPane.getTitleAt(i).replace("#FF0000", "#FFFFFF");
+			newTitle = newTitle.replaceAll("([A-Z])*Selected", "");
+			tabbedPane.setTitleAt(i, newTitle);
+		}
 	}
 }
