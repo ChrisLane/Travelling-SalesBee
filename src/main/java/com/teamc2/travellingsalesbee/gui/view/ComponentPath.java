90e82a20d64b62fff1f7ffd1053c71210de7b5d3
Neil Farrington <NFarrington@users.noreply.github.com>
2016-03-11 13:26:37 +0000

    Base code for 2opt visualisation

--- src/main/java/com/teamc2/travellingsalesbee/gui/view/ComponentPath.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/view/ComponentPath.java
@@ -40,8 +40,8 @@ public class ComponentPath extends JComponent {
 	public void setPath(ArrayList<Cell> path) {
 		ArrayList<Cell> steppedPath = new ArrayList<>();
 	}
-	
-	public void setMap(Map map){
+
+	public void setMap(Map map) {
 		this.map = map;
 	}
 
@@ -49,13 +49,14 @@ public class ComponentPath extends JComponent {
 		this.antSteps = antSteps;
 		repaint();
 	}
+
 	//Naive visualisation
 	public void setNaiveSteps(ArrayList<NaiveStep> steps) {
 		naiveSteps = steps;
 		repaint();
 	}
-	
-	public ArrayList<NaiveStep> getNaiveSteps(){
+
+	public ArrayList<NaiveStep> getNaiveSteps() {
 		return this.naiveSteps;
 	}
 
@@ -63,8 +64,8 @@ public class ComponentPath extends JComponent {
 		this.experimentalSteps = experimentalSteps;
 		repaint();
 	}
-	
-	public ArrayList<ExperimentalStep> getExperimentalSteps(){
+
+	public ArrayList<ExperimentalStep> getExperimentalSteps() {
 		return this.experimentalSteps;
 	}
 
@@ -72,16 +73,16 @@ public class ComponentPath extends JComponent {
 		this.stepNum = stepNum;
 		repaint();
 	}
-	
+
 	public void setAlgorithmType(AlgorithmType type) {
 		this.type = type;
 		this.repaint();
 	}
-	
+
 	public String getNaiveStepText(int stepNum) {
 		return this.naiveSteps.get(stepNum).getText();
 	}
-	
+
 	public ArrayList<Cell> getBeePath() {
 		return beePath;
 	}
@@ -90,22 +91,22 @@ public class ComponentPath extends JComponent {
 	public void paint(Graphics g) {
 		super.paint(g);
 		Graphics2D g2 = (Graphics2D) g;
-		
+
 		switch (this.type) {
-		case BEE:
-			paintBeePath(g2);
-			break;
-		case ANT:
-			paintAntPath(g2);
-			break;
-		case NEARESTNEIGHBOUR:
-			paintNearestNeighbourPath(g2);
-			break;
-		case TWOOPT:
-			paintTwoOptSwapPath(g2);
-			break;
+			case BEE:
+				paintBeePath(g2);
+				break;
+			case ANT:
+				paintAntPath(g2);
+				break;
+			case NEARESTNEIGHBOUR:
+				paintNearestNeighbourPath(g2);
+				break;
+			case TWOOPT:
+				paintTwoOptSwapPath(g2);
+				break;
 		}
-		
+
 		if (beePath.size() > 0 && stepNum >= (naiveSteps.size() + experimentalSteps.size()) - 2) {
 
 			int x1, x2 = 0, y1, y2 = 0;
@@ -140,14 +141,43 @@ public class ComponentPath extends JComponent {
 	}
 
 	private void paintTwoOptSwapPath(Graphics2D g2) {
-		// TODO Auto-generated method stub
-		
+		if (naiveSteps.size() > 0 && stepNum < naiveSteps.size()) {
+			int x1, x2, y1, y2;
+
+			for (int i = 0; i < stepNum + 1; i++) {
+				if (i < naiveSteps.size()) {
+					NaiveStep step = naiveSteps.get(i);
+					x1 = (int) step.getStart().x;
+					y1 = (int) step.getStart().y;
+
+					ArrayList<Cell> available = step.getAvailable();
+
+					if (i == stepNum) {
+						for (Cell anAvailable : available) {
+							g2.setStroke(new BasicStroke(5));
+							g2.setPaint(Color.red);
+							x2 = (int) anAvailable.x;
+							y2 = (int) anAvailable.y;
+							g2.drawLine(x1 + 25, y1 + 25, x2 + 25, y2 + 25);
+						}
+					}
+
+					g2.setStroke(new BasicStroke(6));
+					g2.setPaint(Color.green);
+					x2 = (int) step.getEnd().x;
+					y2 = (int) step.getEnd().y;
+					g2.drawLine(x1 + 25, y1 + 25, x2 + 25, y2 + 25);
+				}
+			}
+		} else if (stepNum >= naiveSteps.size()) {
+			System.out.println("Do swaps");
+		}
 	}
 
 	private void paintNearestNeighbourPath(Graphics2D g2) {
 		if (naiveSteps.size() > 0 && stepNum < naiveSteps.size()) {
 			int x1, x2, y1, y2;
-			
+
 			for (int i = 0; i < stepNum + 1; i++) {
 				if (i < naiveSteps.size()) {
 					NaiveStep step = naiveSteps.get(i);
@@ -174,13 +204,13 @@ public class ComponentPath extends JComponent {
 				}
 			}
 		}
-		
+
 	}
 
-	private void paintBeePath(Graphics2D g2){
+	private void paintBeePath(Graphics2D g2) {
 		if (naiveSteps.size() > 0 && stepNum < naiveSteps.size()) {
 			int x1, x2, y1, y2;
-			
+
 			for (int i = 0; i < stepNum + 1; i++) {
 				if (i < naiveSteps.size()) {
 					NaiveStep step = naiveSteps.get(i);
@@ -226,14 +256,14 @@ public class ComponentPath extends JComponent {
 					y2 = (int) stepPath.get(i + 1).y;
 				} else {
 					lineColor = Color.RED;
-					ExperimentalStep step2 = experimentalSteps.get(stepNum - naiveSteps.size() -1);
+					ExperimentalStep step2 = experimentalSteps.get(stepNum - naiveSteps.size() - 1);
 					ArrayList<Cell> stepPath2 = step2.getPath();
 					x1 = (int) stepPath2.get(i).x;
 					y1 = (int) stepPath2.get(i).y;
 					x2 = (int) stepPath2.get(i + 1).x;
 					y2 = (int) stepPath2.get(i + 1).y;
 				}
-				
+
 				g2.setPaint(lineColor);
 				g2.setStroke(new BasicStroke(5));
 				g2.drawLine(x1 + 25, y1 + 25, x2 + 25, y2 + 25);
@@ -258,24 +288,24 @@ public class ComponentPath extends JComponent {
 			g2.drawString(costString, 0, 0);
 		}
 	}
-	
-	private void paintAntPath(Graphics2D g2){
-		if (antSteps.size() > 0){
+
+	private void paintAntPath(Graphics2D g2) {
+		if (antSteps.size() > 0) {
 			g2.setPaint(Color.black);
 			g2.setStroke(new BasicStroke(5));
 			g2.drawLine(0, 0, 50, 50);
-			
+
 			int x1, x2, y1, y2;
 			AntStep step = antSteps.get(stepNum);
 			ArrayList<Cell> path = step.getPath();
-			
-			for (int i=0;i<path.size();i++){
+
+			for (int i = 0; i < path.size(); i++) {
 				x1 = (int) path.get(i).getX();
 				y1 = (int) path.get(i).getY();
-				for (int j=(i+1);j<path.size();j++){
+				for (int j = (i + 1); j < path.size(); j++) {
 					x2 = (int) path.get(j).getX();
 					y2 = (int) path.get(j).getY();
-					Color lineColor = new Color(255, 255, 0, (int) ((255/path.size())*map.getCostMatrix().getPheromone(path.get(i), path.get(j))));
+					Color lineColor = new Color(255, 255, 0, (int) ((255 / path.size()) * map.getCostMatrix().getPheromone(path.get(i), path.get(j))));
 
 					g2.setPaint(lineColor);
 					g2.setStroke(new BasicStroke(5));
@@ -283,10 +313,8 @@ public class ComponentPath extends JComponent {
 				}
 			}
 		}
-		
-	}
 
-	
+	}
 
 
 }
