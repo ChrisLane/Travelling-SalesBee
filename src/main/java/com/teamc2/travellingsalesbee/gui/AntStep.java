cca8d52ce7eabe505f92f33b3f276a8ea91cdcf8
Bradley Rowe <bmjr@users.noreply.github.com>
2016-03-11 12:47:56 +0000

    Began to visualise the ant steps

---
+++ src/main/java/com/teamc2/travellingsalesbee/gui/AntStep.java
@@ -0,0 +1,19 @@
+package com.teamc2.travellingsalesbee.gui;
+
+import java.util.ArrayList;
+
+import com.teamc2.travellingsalesbee.gui.data.cells.Cell;
+
+public class AntStep {
+
+	private ArrayList<Cell> path = new ArrayList<>();
+
+	public AntStep(ArrayList<Cell> path) {
+		this.path = path;
+	}
+	
+	public ArrayList<Cell> getPath() {
+		return this.path;
+	}
+
+}
