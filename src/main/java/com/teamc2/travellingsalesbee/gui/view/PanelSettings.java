7c1dd4d1a82d10c9e9ef60821a248324a6356a0f
npf489 <npf489@7a56c92f-03fb-4445-8927-fcf652c9610f>
2016-03-11 13:26:37 +0000

    Base code for 2opt visualisation

--- src/main/java/com/teamc2/travellingsalesbee/gui/view/PanelSettings.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/view/PanelSettings.java
@@ -23,6 +23,7 @@ import com.teamc2.travellingsalesbee.algorithms.Ant;
 import com.teamc2.travellingsalesbee.algorithms.Bee;
 import com.teamc2.travellingsalesbee.algorithms.NearestNeighbour;
 import com.teamc2.travellingsalesbee.gui.AntStep;
+import com.teamc2.travellingsalesbee.algorithms.TwoOptSwap;
 import com.teamc2.travellingsalesbee.gui.ExperimentalStep;
 import com.teamc2.travellingsalesbee.gui.NaiveStep;
 import com.teamc2.travellingsalesbee.gui.data.Map;
@@ -127,7 +128,7 @@ public class PanelSettings extends JPanel {
 		// Size of parent-panel
 		txtPaneTextWillAppear.setBackground(new Color(0, 0, 0, 0));
 		txtPaneTextWillAppear.setOpaque(false);
-		
+
 		Platform.runLater(() -> {
 			Pane root = new Pane();
 			root.setId("infoboxPanel");
@@ -148,9 +149,9 @@ public class PanelSettings extends JPanel {
 
 		btnPrev = new JButton("<-");
 		btnNext = new JButton("->");
-		
+
 		btnPrev.addActionListener(arg0 -> {
-			setStepNum(stepNum-1);
+			setStepNum(stepNum - 1);
 			Platform.runLater(() -> {
 				text.setText(Double.toString(this.distance));
 			});
@@ -166,7 +167,7 @@ public class PanelSettings extends JPanel {
 		});
 
 		btnNext.addActionListener(arg0 -> {
-			setStepNum(stepNum+1);
+			setStepNum(stepNum + 1);
 			Platform.runLater(() -> {
 				setDistance();
 				text.setText(Double.toString(this.distance));
@@ -175,7 +176,7 @@ public class PanelSettings extends JPanel {
 			/*----------------------------------------------*/
 			try {
 				//if (stepNum < experimentalRuns) {
-					panelMap.getPanelAnimalAnimation().setStepNum(stepNum);
+				panelMap.getPanelAnimalAnimation().setStepNum(stepNum);
 				//}
 			} catch (IndexOutOfBoundsException e) {
 				System.err.println("Exception in setting animation");
@@ -194,25 +195,49 @@ public class PanelSettings extends JPanel {
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			switch (type) {
-			case BEE:
-				runBeeAlgorithm();
-				break;
-			case ANT:
-				runAntAlgorithm();
-				break;
-			case NEARESTNEIGHBOUR:
-				runNearestNeighbourAlgorithm();
-				break;
-			case TWOOPT:
-				runTwoOptAlgorithm();
-				break;
+				case BEE:
+					runBeeAlgorithm();
+					break;
+				case ANT:
+					runAntAlgorithm();
+					break;
+				case NEARESTNEIGHBOUR:
+					runNearestNeighbourAlgorithm();
+					break;
+				case TWOOPT:
+					runTwoOptAlgorithm();
+					break;
 			}
-			
 		}
 
 		private void runTwoOptAlgorithm() {
-			// TODO Auto-generated method stub
-			
+			try {
+				setStepNum(0);
+				map.setCostMatrix();
+				TwoOptSwap tos = new TwoOptSwap(map, 2);
+				BeeVisualiser visualise = new BeeVisualiser();
+				tos.naiveRun();
+				tos.swapRun();
+				System.out.println("TOS path size: " + tos.getPath().size());
+				ArrayList<NaiveStep> steps = visualise.getNaiveSteps(tos.getPath());
+				panelMap.getPathComponent().setNaiveSteps(steps);
+
+				ArrayList<ArrayList<Cell>> pathOfPaths = new ArrayList<>();
+				ArrayList<Cell> hive = new ArrayList<>();
+				hive.add(steps.get(0).getStart());
+				pathOfPaths.add(hive);
+
+				for (NaiveStep naiveStep : steps) {
+					ArrayList<Cell> singlePoint = new ArrayList<>();
+					singlePoint.add(naiveStep.getEnd());
+					pathOfPaths.add(singlePoint);
+				}
+
+				panelMap.getPanelAnimalAnimation().setPathofPaths(pathOfPaths);
+			} catch (NullPointerException e) {
+				btnPrev.setEnabled(false);
+				btnNext.setEnabled(false);
+			}
 		}
 
 		private void runNearestNeighbourAlgorithm() {
@@ -225,15 +250,15 @@ public class PanelSettings extends JPanel {
 				nearestNeighbour.naiveRun();
 				ArrayList<NaiveStep> naiveSteps = visualise.getNaiveSteps(nearestNeighbour.getPath());
 				panelMap.getPathComponent().setNaiveSteps(naiveSteps);
-	
+
 				ArrayList<ArrayList<Cell>> pathOfPaths = new ArrayList<>();
 				ArrayList<Cell> hive = new ArrayList<>();
 				hive.add(naiveSteps.get(0).getStart());
 
-	
+
 				for (NaiveStep naiveStep : naiveSteps) {
 					hive.add(naiveStep.getEnd());
-					
+
 				}
 				pathOfPaths.add(hive);
 	
@@ -245,15 +270,15 @@ public class PanelSettings extends JPanel {
 				// When no nodes are on the screen: disable the buttons.
 				btnPrev.setEnabled(false);
 				btnNext.setEnabled(false);
-				
+
 			}
-			
+
 		}
 
 		private void runAntAlgorithm() {
 			Ant ant = new Ant(map);
 			ArrayList<ArrayList<Cell>> setOfRuns = new ArrayList();
-			for (int i=0;i<10;i++){
+			for (int i = 0; i < 10; i++) {
 				ant.pheromoneRun();
 				setOfRuns.add(ant.getPath());
 			}
@@ -261,35 +286,35 @@ public class PanelSettings extends JPanel {
 			ArrayList<AntStep> antSteps = visualise.getAntSteps(setOfRuns);
 			panelMap.getPathComponent().setMap(map);
 			panelMap.getPathComponent().setAntSteps(antSteps);
-			
+
 		}
 
 		private void runBeeAlgorithm() {
 			try {
 				setStepNum(0);
-				
+
 				map.setCostMatrix();
 				Bee bee = new Bee(map, experimentalRuns);
 				BeeVisualiser visualise = new BeeVisualiser();
 				bee.naiveRun();
 				ArrayList<NaiveStep> naiveSteps = visualise.getNaiveSteps(bee.getPath());
 				panelMap.getPathComponent().setNaiveSteps(naiveSteps);
-	
+
 				bee.experimentalRun();
 				ArrayList<ExperimentalStep> experimentalSteps = visualise.getExperimentalSteps(bee.getCellComparisons(), bee.getIntermediaryPaths(), bee.getIntermediaryPathCosts());
 				panelMap.getPathComponent().setExperimentalSteps(experimentalSteps);
 				panelMap.getPathComponent().setPath(bee.getPath()); // THIS DOES NOTHING
-	
+
 				ArrayList<ArrayList<Cell>> pathOfPaths = new ArrayList<>();
 				ArrayList<Cell> hive = new ArrayList<>();
 				hive.add(naiveSteps.get(0).getStart());
-	
+
 				for (NaiveStep naiveStep : naiveSteps) {
 					hive.add(naiveStep.getEnd());
-					
+
 				}
 				pathOfPaths.add(hive);
-	
+
 				for (ExperimentalStep experimentalStep : experimentalSteps) {
 					ArrayList<Cell> setOfPoints = experimentalStep.getPath();
 					pathOfPaths.add(setOfPoints);
@@ -306,23 +331,23 @@ public class PanelSettings extends JPanel {
 			}
 		}
 	}
-	
+
 	public void setStepNum(int stepNum) {
 		this.stepNum = stepNum;
 		panelMap.getPathComponent().setStepNumber(stepNum);
 		btnPrev.setEnabled((stepNum > 0));
 		btnNext.setEnabled((stepNum != -1));
 	}
-	
-	public void setAlgorithmType(AlgorithmType type){
+
+	public void setAlgorithmType(AlgorithmType type) {
 		this.type = type;
 	}
 
 	private void setDistance() {
 		double distance = 0;
-		switch(type){
+		switch (type) {
 			case BEE:
-				if (stepNum < panelMap.getPathComponent().getNaiveSteps().size()){
+				if (stepNum < panelMap.getPathComponent().getNaiveSteps().size()) {
 					NaiveStep step = panelMap.getPathComponent().getNaiveSteps().get(stepNum);
 					distance = step.getStart().distance(step.getEnd());
 				}
@@ -333,9 +358,9 @@ public class PanelSettings extends JPanel {
 				break;
 			case TWOOPT:
 				break;
-	}
+		}
 		this.distance = distance;
 	}
-	
-	
+
+
 }
