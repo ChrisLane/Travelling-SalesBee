e4df9dbc8b11bba2655864741fbd01f0d5b9cbca
mxm499 <Sefree6@users.noreply.github.com>
2016-03-16 12:57:31 +0000

    Added more methods to customise the text

--- src/main/java/com/teamc2/travellingsalesbee/gui/view/PanelOverlyingText.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/view/PanelOverlyingText.java
@@ -1,9 +1,12 @@
 package com.teamc2.travellingsalesbee.gui.view;
 
+import com.teamc2.travellingsalesbee.gui.data.cells.Cell;
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
 import javafx.scene.Scene;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
@@ -40,15 +43,18 @@ public class PanelOverlyingText extends JFXPanel {
 			// Initialising the scene
 			scene = new Scene(root, width, height);
 			scene.setFill(javafx.scene.paint.Color.rgb(0, 0, 0, 0)); //Set it to transparent
-			URL url1 = this.getClass().getResource("/assets/stylesheets/visualiser.css");
-			scene.getStylesheets().add(url1.toExternalForm());
 
-			// Initialising the text, settings its font and initial text
+			// Initialising the text, settings its font and initial text and giving it a drop shadow
 			text = new Text();
 			setSize(80);
-			text.setId("panelText");
+			setTextColor(Color.color(1.0, 1.0, 1.0, 1.0));
+			setDropShadow(true);
 			setTextXandY(0, 95);
 			this.setText("Initial text");
+
+			//Set the stylesheet for the scene (make textPane transparent)
+			URL url = this.getClass().getResource("/assets/stylesheets/visualiser.css");
+			scene.getStylesheets().add(url.toExternalForm());
 
 			root.getChildren().add(text);
 			setScene(scene);
@@ -71,6 +77,24 @@ public class PanelOverlyingText extends JFXPanel {
 		Platform.runLater(() -> {
 			text.setX(x);
 			text.setY(y);
+		});
+	}
+
+	public void setTextColor(Color color) {
+		Platform.runLater(() -> {
+			text.setFill(color);
+		});
+	}
+
+	public void setDropShadow(boolean bool) {
+		Platform.runLater(() -> {
+			if(bool) {
+				DropShadow ds = new DropShadow();
+				ds.setOffsetY(3.0f);
+				ds.setColor(Color.color(0.4f, 0.4f, 0.4f));
+
+				text.setEffect(ds);
+			}
 		});
 	}
 }
