594a0e3bffe9f894c0c1487ed136b3761aaf1550
cml476 <cml476@7a56c92f-03fb-4445-8927-fcf652c9610f>
2016-03-23 18:15:05 +0000

    Move test DataProviders to a seperate class

--- src/test/java/com/teamc2/travellingsalesbee/algorithms/BeeTest.java
+++ src/test/java/com/teamc2/travellingsalesbee/algorithms/BeeTest.java
@@ -1,10 +1,11 @@
 package com.teamc2.travellingsalesbee.algorithms;
 
 import com.teamc2.travellingsalesbee.gui.data.Map;
-import com.teamc2.travellingsalesbee.gui.data.cells.*;
+import com.teamc2.travellingsalesbee.gui.data.cells.Cell;
+import com.teamc2.travellingsalesbee.gui.data.cells.CellComparator;
+import com.teamc2.travellingsalesbee.gui.data.cells.CellNode;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
@@ -18,66 +19,11 @@ public class BeeTest {
 	private Bee bee;
 	private Map map;
 
-	/**
-	 * Setup data for the tests
-	 */
 	@BeforeClass
 	public void initialise() {
-		map = new Map();
-		map.setCell(0, 0, CellType.ORIGIN);
-		map.setCell(0, 2, CellType.NODE); // flower 1
-		map.setCell(0, 6, CellType.NODE); // flower 2
-		map.setCell(2, 6, CellType.NODE); // flower 3
-
+		Data data = new Data();
+		map = data.getMap();
 		bee = new Bee(map, 20);
-	}
-
-	/**
-	 * Paths and costs to be used in tests
-	 *
-	 * @return Array of paths and their costs to be used in tests
-	 */
-	@DataProvider(name = "paths")
-	public Object[][] path() {
-		ArrayList<CellNode> flowers = map.getNodes();
-		CellNode flower1 = flowers.get(0);
-		CellNode flower2 = flowers.get(1);
-		CellNode flower3 = flowers.get(2);
-		CellOrigin hive = map.getOrigin();
-
-		double path1Cost = 14.32455532033676;
-		ArrayList<Cell> path1 = new ArrayList<>();
-		path1.add(hive);
-		path1.add(flower1); // cost 2
-		path1.add(flower2); // cost 4
-		path1.add(flower3); // cost 2
-		path1.add(hive); // cost 6.32455532033676
-
-		double path2Cost = 14.32455532033676;
-		ArrayList<Cell> path2 = new ArrayList<>();
-		path2.add(hive);
-		path2.add(flower3); // cost 6.324555397033691
-		path2.add(flower2); // cost 2
-		path2.add(flower1); // cost 4
-		path2.add(hive); // cost 2
-
-		double path3Cost = 20.79669127533634;
-		ArrayList<Cell> path3 = new ArrayList<>();
-		path3.add(hive);
-		path3.add(flower2); // cost 6
-		path3.add(flower1); // cost 4
-		path3.add(flower3); // cost 4.4721360206604
-		path3.add(hive); // cost 6.32455532033676
-
-		double path4Cost = 20.79669127533634;
-		ArrayList<Cell> path4 = new ArrayList<>();
-		path4.add(hive);
-		path4.add(flower3); // cost 6.32455532033676
-		path4.add(flower1); // cost 4.4721360206604
-		path4.add(flower2); // cost 4
-		path4.add(hive); // cost 6
-
-		return new Object[][]{{path1, path1Cost}, {path2, path2Cost}, {path3, path3Cost}, {path4, path4Cost}};
 	}
 
 	/**
@@ -113,7 +59,7 @@ public class BeeTest {
 	 * @param path Input paths
 	 * @param cost Costs to be matched
 	 */
-	@Test(dataProvider = "paths")
+	@Test(dataProvider = "paths", dataProviderClass = Data.class)
 	public void testCalculatePathCost(ArrayList<Cell> path, double cost) throws Exception {
 		Assert.assertEquals(bee.calculatePathCost(path), cost);
 	}
@@ -124,7 +70,7 @@ public class BeeTest {
 	 * @param path Initial path
 	 * @param cost Initial cost
 	 */
-	@Test(dataProvider = "paths", dependsOnMethods = {"testSetPath", "testSetPathCost"})
+	@Test(dataProvider = "paths", dataProviderClass = Data.class, dependsOnMethods = {"testSetPath", "testSetPathCost"})
 	public void testExperimentalRuns(ArrayList<Cell> path, double cost) throws Exception {
 		bee.setPath(path, cost);
 		bee.experimentalRun();
@@ -137,7 +83,7 @@ public class BeeTest {
 	 * @param path Path to set
 	 * @param cost Path cost to set
 	 */
-	@Test(dataProvider = "paths")
+	@Test(dataProvider = "paths", dataProviderClass = Data.class)
 	public void testSetPath(ArrayList<Cell> path, double cost) throws Exception {
 		bee.setPath(path, cost);
 		Assert.assertEquals(bee.getPath(), path);
@@ -149,7 +95,7 @@ public class BeeTest {
 	 * @param path Path to set
 	 * @param cost Path cost to set
 	 */
-	@Test(dataProvider = "paths")
+	@Test(dataProvider = "paths", dataProviderClass = Data.class)
 	public void testSetPathCost(ArrayList<Cell> path, double cost) throws Exception {
 		bee.setPath(path, cost);
 		Assert.assertEquals(bee.getCost(), cost);
---
+++ src/test/java/com/teamc2/travellingsalesbee/algorithms/Data.java
@@ -0,0 +1,79 @@
+package com.teamc2.travellingsalesbee.algorithms;
+
+import com.teamc2.travellingsalesbee.gui.data.Map;
+import com.teamc2.travellingsalesbee.gui.data.cells.Cell;
+import com.teamc2.travellingsalesbee.gui.data.cells.CellNode;
+import com.teamc2.travellingsalesbee.gui.data.cells.CellOrigin;
+import com.teamc2.travellingsalesbee.gui.data.cells.CellType;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+
+import java.util.ArrayList;
+
+public class Data {
+	private static Map map;
+
+	/**
+	 * Setup data for the tests
+	 */
+	@BeforeClass
+	public void initialise() {
+		map = new Map();
+		map.setCell(0, 0, CellType.ORIGIN);
+		map.setCell(0, 2, CellType.NODE); // flower 1
+		map.setCell(0, 6, CellType.NODE); // flower 2
+		map.setCell(2, 6, CellType.NODE); // flower 3
+	}
+
+	public Map getMap() {
+		return map;
+	}
+
+	/**
+	 * Paths and costs to be used in tests
+	 *
+	 * @return Array of paths and their costs to be used in tests
+	 */
+	@DataProvider(name = "paths")
+	public static Object[][] path() {
+		ArrayList<CellNode> flowers = map.getNodes();
+		CellNode flower1 = flowers.get(0);
+		CellNode flower2 = flowers.get(1);
+		CellNode flower3 = flowers.get(2);
+		CellOrigin hive = map.getOrigin();
+
+		double path1Cost = 14.32455532033676;
+		ArrayList<Cell> path1 = new ArrayList<>();
+		path1.add(hive);
+		path1.add(flower1); // cost 2
+		path1.add(flower2); // cost 4
+		path1.add(flower3); // cost 2
+		path1.add(hive); // cost 6.32455532033676
+
+		double path2Cost = 14.32455532033676;
+		ArrayList<Cell> path2 = new ArrayList<>();
+		path2.add(hive);
+		path2.add(flower3); // cost 6.324555397033691
+		path2.add(flower2); // cost 2
+		path2.add(flower1); // cost 4
+		path2.add(hive); // cost 2
+
+		double path3Cost = 20.79669127533634;
+		ArrayList<Cell> path3 = new ArrayList<>();
+		path3.add(hive);
+		path3.add(flower2); // cost 6
+		path3.add(flower1); // cost 4
+		path3.add(flower3); // cost 4.4721360206604
+		path3.add(hive); // cost 6.32455532033676
+
+		double path4Cost = 20.79669127533634;
+		ArrayList<Cell> path4 = new ArrayList<>();
+		path4.add(hive);
+		path4.add(flower3); // cost 6.32455532033676
+		path4.add(flower1); // cost 4.4721360206604
+		path4.add(flower2); // cost 4
+		path4.add(hive); // cost 6
+
+		return new Object[][]{{path1, path1Cost}, {path2, path2Cost}, {path3, path3Cost}, {path4, path4Cost}};
+	}
+}
