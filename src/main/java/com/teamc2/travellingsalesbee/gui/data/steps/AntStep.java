527374423cb2524b20ad207f0a932f48a587c2d2
cml476 <cml476@7a56c92f-03fb-4445-8927-fcf652c9610f>
2016-03-23 13:04:09 +0000

    JavaDocs for steps

--- src/main/java/com/teamc2/travellingsalesbee/gui/data/steps/AntStep.java
+++ src/main/java/com/teamc2/travellingsalesbee/gui/data/steps/AntStep.java
@@ -4,11 +4,11 @@ import com.teamc2.travellingsalesbee.algorithms.cost.CostMatrix;
 import com.teamc2.travellingsalesbee.gui.data.cells.Cell;
 
 import java.util.ArrayList;
+
 /**
- * A class to represent a single pheremone run from the ant algorithm
- * 
- * @author Bradley Rowe (bmr455)
+ * A class to represent a single pheromone run from the ant algorithm
  *
+ * @author Bradley Rowe (bmr455)
  */
 public class AntStep {
 
@@ -17,9 +17,9 @@ public class AntStep {
 
 	/**
 	 * Constructs a step of ant algorithm
-	 * 
-	 * @param path The path produced by the pheremone run
-	 * @param costMatrix The cost matrix produced by the pheremone run
+	 *
+	 * @param path       The path produced by the pheromone run
+	 * @param costMatrix The cost matrix produced by the pheromone run
 	 */
 	public AntStep(ArrayList<Cell> path, CostMatrix costMatrix) {
 		this.path = path;
@@ -27,7 +27,8 @@ public class AntStep {
 	}
 
 	/**
-	 * 
+	 * Get the path of the step
+	 *
 	 * @return path The path stored in this step
 	 */
 	public ArrayList<Cell> getPath() {
@@ -35,7 +36,8 @@ public class AntStep {
 	}
 
 	/**
-	 * 
+	 * Get the cost matrix of the step
+	 *
 	 * @return costMatrix The cost matrix stored in this step
 	 */
 	public CostMatrix getCostMatrix() {
