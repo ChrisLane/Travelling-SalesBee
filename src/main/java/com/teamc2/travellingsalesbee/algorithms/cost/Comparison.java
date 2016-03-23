b1c358eeeba02ce03adca2288849f3c81bb265ea
Chris Lane <ChrisLane@users.noreply.github.com>
2016-03-23 03:40:34 +0000

    cost package is now JavaDoc'd

--- src/main/java/com/teamc2/travellingsalesbee/algorithms/cost/Comparison.java
+++ src/main/java/com/teamc2/travellingsalesbee/algorithms/cost/Comparison.java
@@ -2,20 +2,18 @@ package com.teamc2.travellingsalesbee.algorithms.cost;
 
 /**
  * A class used to instantiate a comparison between two cells
- * Currently implemented in the experimental steps as each step contains two compared nodes
- * 
- * @author Bradley Rowe (bmr455)
  *
  * @param <Cell1> The first cell to compare against
  * @param <Cell2> The second cell to compare against the first
+ * @author Bradley Rowe (bmr455)
  */
 public class Comparison<Cell1, Cell2> {
 	private final Cell1 cell1;
 	private final Cell2 cell2;
 
 	/**
-	 * Constructs a comparison between that stores two objects passed into it 
-	 * 
+	 * Constructs a comparison between that stores two objects passed into it
+	 *
 	 * @param cell1 The first cell passed into the comparison.
 	 * @param cell2 The second cell passed into the comparison.
 	 */
@@ -25,14 +23,14 @@ public class Comparison<Cell1, Cell2> {
 	}
 
 	/**
-	 * @return cell1. The first cell to compare the second one against
+	 * @return cell1. The first cell in a comparison
 	 */
 	public Cell1 getCell1() {
 		return cell1;
 	}
 
 	/**
-	 * @return cell2. The second cell to compare the first one against
+	 * @return cell2. The second cell in a comparison
 	 */
 	public Cell2 getCell2() {
 		return cell2;
