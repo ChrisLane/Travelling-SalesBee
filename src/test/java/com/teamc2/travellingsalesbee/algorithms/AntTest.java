c982d670c4be3665434085bae57f1103fa892702
Chris Lane <ChrisLane@users.noreply.github.com>
2016-03-23 19:17:05 +0000

    Move inherited method tests from Bee to NearestNeighbour

---
+++ src/test/java/com/teamc2/travellingsalesbee/algorithms/AntTest.java
@@ -0,0 +1,10 @@
+package com.teamc2.travellingsalesbee.algorithms;
+
+/**
+ * A test class for the ant colony algorithm
+ *
+ * @author Christopher Lane (cml476)
+ */
+public class AntTest {
+
+}
--- src/test/java/com/teamc2/travellingsalesbee/algorithms/BeeTest.java
+++ src/test/java/com/teamc2/travellingsalesbee/algorithms/BeeTest.java
@@ -2,9 +2,8 @@ package com.teamc2.travellingsalesbee.algorithms;
 
 import com.teamc2.travellingsalesbee.gui.data.Map;
 import com.teamc2.travellingsalesbee.gui.data.cells.Cell;
-import com.teamc2.travellingsalesbee.gui.data.cells.CellComparator;
-import com.teamc2.travellingsalesbee.gui.data.cells.CellNode;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -15,53 +14,18 @@ import java.util.ArrayList;
  *
  * @author Christopher Lane (cml476)
  */
+@Test(dependsOnGroups = "NearestNeighbour")
 public class BeeTest {
 	private Bee bee;
-	private Map map;
 
+	/**
+	 * Setup the environment for testing
+	 */
 	@BeforeClass
 	public void initialise() {
 		Data data = new Data();
-		map = data.getMap();
+		Map map = data.getMap();
 		bee = new Bee(map, 20);
-	}
-
-	/**
-	 * Test that every node is contained in the path generated
-	 */
-	@Test(dependsOnMethods = {"testSetPath", "testSetPathCost", "testCalculatePathCost"})
-	public void testNaiveRun() throws Exception {
-		// TODO: Change this to include the origin
-		// Set the path to empty
-		bee.setPath(new ArrayList<>(), 0);
-
-		// Generate a new path
-		bee.naiveRun();
-
-		// Sort the path and remove origin
-		ArrayList<Cell> path = bee.getPath();
-		// Remove start (origin)
-		path.remove(0);
-		// Remove finish (origin)
-		path.remove(path.size() - 1);
-		path.sort(new CellComparator());
-
-		// Sort the flowers
-		ArrayList<CellNode> flowers = map.getNodes();
-		flowers.sort(new CellComparator());
-
-		Assert.assertEquals(path.equals(flowers), true);
-	}
-
-	/**
-	 * Test that costs are being calculated correctly
-	 *
-	 * @param path Input paths
-	 * @param cost Costs to be matched
-	 */
-	@Test(dataProvider = "paths", dataProviderClass = Data.class)
-	public void testCalculatePathCost(ArrayList<Cell> path, double cost) throws Exception {
-		Assert.assertEquals(bee.calculatePathCost(path), cost);
 	}
 
 	/**
@@ -70,34 +34,10 @@ public class BeeTest {
 	 * @param path Initial path
 	 * @param cost Initial cost
 	 */
-	@Test(dataProvider = "paths", dataProviderClass = Data.class, dependsOnMethods = {"testSetPath", "testSetPathCost"})
+	@Test(dataProvider = "paths", dataProviderClass = Data.class)
 	public void testExperimentalRuns(ArrayList<Cell> path, double cost) throws Exception {
 		bee.setPath(path, cost);
 		bee.experimentalRun();
 		Assert.assertTrue(bee.getCost() <= cost);
-	}
-
-	/**
-	 * Test that the stored path is the same as the path we set
-	 *
-	 * @param path Path to set
-	 * @param cost Path cost to set
-	 */
-	@Test(dataProvider = "paths", dataProviderClass = Data.class)
-	public void testSetPath(ArrayList<Cell> path, double cost) throws Exception {
-		bee.setPath(path, cost);
-		Assert.assertEquals(bee.getPath(), path);
-	}
-
-	/**
-	 * Test that the stored path cost is the same as the cost we set
-	 *
-	 * @param path Path to set
-	 * @param cost Path cost to set
-	 */
-	@Test(dataProvider = "paths", dataProviderClass = Data.class)
-	public void testSetPathCost(ArrayList<Cell> path, double cost) throws Exception {
-		bee.setPath(path, cost);
-		Assert.assertEquals(bee.getCost(), cost);
 	}
 }
